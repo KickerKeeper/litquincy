c3d6af345495eb89cf7dfe5da3af0af8f6f63a30
Ian Hoffman <ijh6@cornell.edu>
2016-07-04 16:16:00 -0400

    Table rendering works

--- app/js/report-ajax.js
+++ app/js/report-ajax.js
@@ -1,10 +1,40 @@
+var toTable = function(data) {
+    var tableHtml = "";
+    if (data.length > 0) {
+        // Open table
+        tableHtml += "<table>";
+
+        // Get attributes of first item for column labels
+        tableHtml += "<tr>";
+        var labels = Object.keys( data[0] );
+        for (var i = 0; i < labels.length; i++) {
+            tableHtml += "<td>" + labels[i] + "</td>";
+        }
+        tableHtml += "</tr>";
+
+        // Fill columns with each item's attributes
+        for (var i = 0; i < data.length; i++) {
+            tableHtml += "<tr>";
+            for (var property in data[i]) {
+                tableHtml += "<td>" + data[i][property] + "</td>";
+            }
+            tableHtml += "</tr>";
+        }
+
+        // Close table
+        tableHtml += "</table>";
+    }
+
+    return tableHtml;
+}
+
 $( document ).ready( function () {
     $.ajax({
         url: "/report-raw",
         method: "get",
-        dataType: "text",
+        dataType: "json",
         success: function(data) {
-            $("#dump").text(data);
+            $("#dump").html(toTable(data));
         },
         failure: function() {
             alert("Request timed out or failed.");
