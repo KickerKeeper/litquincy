90c6ca47b8fd38c74503b657a13db88e2c1df5f8
Ian Hoffman <ijh6@cornell.edu>
2016-07-04 14:22:59 -0400

    Basic test page for downloading CSV

--- index.js
+++ index.js
@@ -10,15 +10,19 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/app/index.html');
+    response.sendFile(__dirname + '/app/index.html');
 });
 
 app.get('/TestResults', function(request, response) {
-  response.sendFile(__dirname + '/app/SpecRunner.html');
+    response.sendFile(__dirname + '/app/SpecRunner.html');
+});
+
+app.get('/CSV', function(request, response) {
+    response.sendFile(__dirname + '/app/ToCSV.html');
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
 
 
